9df40fae5b65d5b3a3b9048564e0cf72f95bcbb6
Sean <keen.coffee2501@fourteen10.com>
2026-06-16 14:06:10 -0700

    fix: Correct template response parameters in index route

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
 @app.get("/api/search")
